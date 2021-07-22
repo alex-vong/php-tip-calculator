479b4a744ed5c09ef9d702f9d933f592bbfd98a3
Alex Vong <avong003@gmail.com>
2021-07-22 01:40:37 -0700

    head

---
+++ head.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>PHP Tip Calculator</title>
+		<meta
+			name="description"
+			content="PHP Tip Calculator"
+		/>
+		<meta
+			name="og:description"
+			content="PHP Tip Calculator"
+		/>
+	
+		<meta property="og:title" content="Never struggle with calculating tips every again!" />
+		<meta property="og:type" content="">
+		<meta property="og:url" content="">
+
+		<meta property="og:image" content="https://peprojects.dev/alpha-1/alexvong/images/tips.png" />
+		<link rel="shortcut icon" type="image/jpg" href="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Circle-icons-calculator.svg/1200px-Circle-icons-calculator.svg.png" />
+
+		<link rel="stylesheet" href="styles/styles.css" />
+	
+</head>
